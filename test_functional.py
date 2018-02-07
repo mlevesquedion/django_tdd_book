695b013cd509febe498b8f64a2378a3eff80262b
Michael <michael.levesque-dion.1@ulaval.ca>
2018-02-06 22:28:35 -0500

    Refactor functional tests to eliminate duplication

--- test_functional.py
+++ test_functional.py
@@ -21,37 +21,40 @@ def test_title(browser):
     assert 'To-Do' in browser.title
 
 
+def enter_new_item(browser, item_text):
+    inputbox = browser.find_element_by_id('id_new_item')
+    inputbox.send_keys(item_text)
+    inputbox.send_keys(Keys.ENTER)
+
+
+def check_for_row_in_list_table(browser, search_text):
+    table = browser.find_element_by_id('id_list_table')
+    rows = table.find_elements_by_tag_name('tr')
+    return any(row.text == search_text for row in rows)
+
+
 def test_edith_story(browser):
 
-    # Edith notices the page header mentions to-do lists
+    # Edith notices the page header mentions to-do lists.
     header_text = browser.find_element_by_tag_name('h1').text
     assert 'To-Do' in header_text
 
-    # She is invited to enter a to-do item straight away
+    # She is invited to enter a to-do item straight away.
     inputbox = browser.find_element_by_id('id_new_item')
     assert inputbox.get_attribute('placeholder') == 'Enter a to-do item'
 
-    # She types "Buy peacock feathers" into a text box
-    inputbox.send_keys('Buy peacock feathers')
-
-    # When she hits Enter, the page updates and now the page lists
-    # "1: Buy peacock feathers" as an item in a to-do list table
-    inputbox.send_keys(Keys.ENTER)
-
-    table = browser.find_element_by_id('id_list_table')
-    rows = table.find_elements_by_tag_name('tr')
-    assert any(row.text == '1: Buy peacock feathers' for row in rows)
+    # She enters an item. The page updates and the item appears in a table.
+    first_item = 'Buy peacock feathers'
+    enter_new_item(browser, first_item)
+    assert check_for_row_in_list_table(browser, '1: {}'.format(first_item))
 
     # There is still a text box inviting her to add another item.
-    # She enters "Use peacock feathers to make a fly"
-    inputbox = browser.find_element_by_id('id_new_item')
-    inputbox.send_keys('Use peacock feathers to make a fly')
-    inputbox.send_keys(Keys.ENTER)
+    # She enters another item.
+    second_item = 'Use peacock feathers to make a fly'
+    enter_new_item(browser, second_item)
 
-    # The page updates again, and now shows both items on her list
-    table = browser.find_element_by_id('id_list_table')
-    rows = table.find_elements_by_tag_name('tr')
-    assert any(row.text == '1: Buy peacock feathers' for row in rows)
-    assert any(row.text == '2: Use peacock feathers to make a fly' for row in rows)
+    # The page updates again, and now shows both items on her list.
+    assert check_for_row_in_list_table(browser, '1: {}'.format(first_item))
+    assert check_for_row_in_list_table(browser, '2: {}'.format(second_item))
 
     assert "You're done" == "Finish the test!!"
