8b69b36d28e4432ebaf3888988d2dcb2ecadf34d
Michael <michael.levesque-dion.1@ulaval.ca>
2018-02-08 22:12:36 -0500

    Djangoify functional tests (previously used pytest)

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,60 +1,71 @@
-import pytest
+import time
+from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 
 
-# Don't forget to start the server first
+MAX_WAIT = 10
 
 
-@pytest.fixture(scope='session')
-def browser():
-    browser = webdriver.Chrome()
-    browser.get('http://localhost:8000')
-    browser.implicitly_wait(2)
+class NewVisitorTest(LiveServerTestCase):
 
-    yield browser
+    def setUp(self):
+        self.browser = webdriver.Chrome()
 
-    browser.close()
+    def tearDown(self):
+        self.browser.quit()
 
 
-def test_title(browser):
-    assert 'To-Do' in browser.title
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
 
-def enter_new_item(browser, item_text):
-    inputbox = browser.find_element_by_id('id_new_item')
-    inputbox.send_keys(item_text)
-    inputbox.send_keys(Keys.ENTER)
+    def test_can_start_a_list_for_one_user(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get(self.live_server_url)
 
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
-def check_for_row_in_list_table(browser, search_text):
-    table = browser.find_element_by_id('id_list_table')
-    rows = table.find_elements_by_tag_name('tr')
-    return any(row.text == search_text for row in rows)
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
 
-def test_edith_story(browser):
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list table
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
-    # Edith notices the page header mentions to-do lists.
-    header_text = browser.find_element_by_tag_name('h1').text
-    assert 'To-Do' in header_text
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very
+        # methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
-    # She is invited to enter a to-do item straight away.
-    inputbox = browser.find_element_by_id('id_new_item')
-    assert inputbox.get_attribute('placeholder') == 'Enter a to-do item'
+        # The page updates again, and now shows both items on her list
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
-    # She enters an item. The page updates and the item appears in a table.
-    first_item = 'Buy peacock feathers'
-    enter_new_item(browser, first_item)
-    assert check_for_row_in_list_table(browser, '1: {}'.format(first_item))
-
-    # There is still a text box inviting her to add another item.
-    # She enters another item.
-    second_item = 'Use peacock feathers to make a fly'
-    enter_new_item(browser, second_item)
-
-    # The page updates again, and now shows both items on her list.
-    assert check_for_row_in_list_table(browser, '1: {}'.format(first_item))
-    assert check_for_row_in_list_table(browser, '2: {}'.format(second_item))
-
-    assert "You're done" == "Finish the test!!"
+        # Satisfied, she goes back to sleep
